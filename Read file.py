9e97d9e099765672c53b411894f70334afe888d8
sheetalmalviya900 <91360201+sheetalmalviya900@users.noreply.github.com>
2021-12-19 00:10:45 +0530

    Add files via upload

---
+++ Read file.py
@@ -0,0 +1,37 @@
+# file=open("people1-exercise.txt","r")
+# my_file=file.read()
+# print(my_file)
+# file.close()
+
+
+
+# file=open("people2-excercise.txt","r")
+# count=0
+# for i in file:
+#     count+=1
+# print(count)
+# file.close()
+
+
+# file=open("people_places.txt","r")
+# my_file=file.read()
+# for i in my_file:
+#     if "delhi" in i:
+#         delhi=open("delhi.txt","a")
+#         delhi.write(i)
+#         delhi.close()
+#     elif "shimla" in i:
+#         sh=open("shimla.txt","a")
+#         sh.write(i)
+#         sh.close()
+#     else:
+#         other=open("other.txt","a")
+#         other.write(i)
+#         other.close()
+# # print(my_file)
+# file.close()
+
+# f=open("sanjana.txt","r")
+# file=f.read()
+# print(file)
+# f.close()
